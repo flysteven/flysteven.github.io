330ee46b06b5290505849dacaf8cabeeae23de36
flysteven <renliang.yuan@gmail.com>
2015-04-20 16:39:50 +0800

    i

--- js/javas.js
+++ js/javas.js
@@ -1,6 +1,6 @@
 window.onload = init;
 function init(){
-	document.getElementById("makeNewTable").onclick = addRowToTable;
+	document.getElementById("jiqiangbing").onclick = addJQB;
 }
 
 function alertTest(){
@@ -8,6 +8,10 @@ function alertTest(){
 }
 
 function loadData(){
+	
+}
+
+function addJQB(){
 	if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
 		xmlhttp=new XMLHttpRequest();
 	}
@@ -18,17 +22,19 @@ function loadData(){
 	xmlhttp.send();
 	xmlDoc=xmlhttp.responseXML; 
 	Engine=xmlDoc.getElementsByTagName("引擎");
-	return Engine;
-}
-
-function addRowToTable(){
-	ItemName=loadData;
-	var tab=document.getElementById("proc"); // 获得表格  
-	var rows=tab.rows; // 表格ROW对象  
-	var row1=tab.insertRow(rows.length); // 插入一行rows是一个数组，代表没一行，索引从0开始  
-	var i=0;
-	row1.insertCell(0).innerHTML=ItemName[i].getElementsByTagName("素质")[0].childNodes[0].nodeValue; // insertCell插入列，从0开始  
-	row1.insertCell(1).innerHTML=ItemName[i].getElementsByTagName("芯片")[0].childNodes[0].nodeValue;  
-	row1.insertCell(2).innerHTML=ItemName[i].getElementsByTagName("空间")[0].childNodes[0].nodeValue;  
-	row1.insertCell(3).innerHTML=ItemName[i].getElementsByTagName("负重")[0].childNodes[0].nodeValue;
+	for(var i=0;i<Engine.length;i++){
+		var tab=document.getElementById("proc"); // 获得表格  
+		var rows=tab.rows; // 表格ROW对象  
+		var row1=tab.insertRow(rows.length); // 插入一行rows是一个数组，代表没一行，索引从0开始  
+		row1.insertCell(0).innerHTML=ItemName[i].getElementsByTagName("序号")[0].childNodes[0].nodeValue; // insertCell插入列，从0开始  
+		row1.insertCell(1).innerHTML=ItemName[i].getElementsByTagName("名称")[0].childNodes[0].nodeValue;  
+		row1.insertCell(2).innerHTML=ItemName[i].getElementsByTagName("素质")[0].childNodes[0].nodeValue;  
+		row1.insertCell(3).innerHTML=ItemName[i].getElementsByTagName("芯片")[0].childNodes[0].nodeValue;
+		row1.insertCell(4).innerHTML=ItemName[i].getElementsByTagName("空间")[0].childNodes[0].nodeValue;
+		row1.insertCell(5).innerHTML=ItemName[i].getElementsByTagName("负重")[0].childNodes[0].nodeValue;
+		row1.insertCell(6).innerHTML=ItemName[i].getElementsByTagName("耐久")[0].childNodes[0].nodeValue;
+		row1.insertCell(7).innerHTML=ItemName[i].getElementsByTagName("耗能")[0].childNodes[0].nodeValue;
+		row1.insertCell(8).innerHTML=ItemName[i].getElementsByTagName("技能")[0].childNodes[0].nodeValue;
+		row1.insertCell(9).innerHTML=ItemName[i].getElementsByTagName("效果")[0].childNodes[0].nodeValue;
+	}
 }
