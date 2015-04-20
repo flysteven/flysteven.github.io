f11b7de3507f87fc1120d762a32d56ce3c41d1f8
flysteven <renliang.yuan@gmail.com>
2015-04-20 16:14:54 +0800

    i

--- js/javas.js
+++ js/javas.js
@@ -1,5 +1,6 @@
 window.onload = init;
 function init(){
+	loadData;
 	document.getElementById("makeNewTable").onclick = addRowToTable;
 }
 
@@ -7,12 +8,26 @@ function alertTest(){
 	alert("this button works!");
 }
 
+function loadData(){
+	if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.open("GET","/dat/机枪兵.xml",false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML; 
+	Engine=xmlDoc.getElementsByTagName("引擎");
+}
+
 function addRowToTable(){
 	var tab=document.getElementById("proc"); // 获得表格  
 	var rows=tab.rows; // 表格ROW对象  
 	var row1=tab.insertRow(rows.length); // 插入一行rows是一个数组，代表没一行，索引从0开始  
-	row1.insertCell(0).innerHTML="0011"; // insertCell插入列，从0开始  
-	row1.insertCell(1).innerHTML="0022";  
-	row1.insertCell(2).innerHTML="0033";  
-	row1.insertCell(3).innerHTML="0044";
+	var i=0;
+	row1.insertCell(0).innerHTML=Engine[i].getElementsByTagName("素质")[0].childNodes[0].nodeValue; // insertCell插入列，从0开始  
+	row1.insertCell(1).innerHTML=Engine[i].getElementsByTagName("芯片")[0].childNodes[0].nodeValue;  
+	row1.insertCell(2).innerHTML=Engine[i].getElementsByTagName("空间")[0].childNodes[0].nodeValue;  
+	row1.insertCell(3).innerHTML=Engine[i].getElementsByTagName("负重")[0].childNodes[0].nodeValue;
 }
