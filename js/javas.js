3bdc4e813bf8d8d1bf845df54d62f97ce1b6b602
flysteven <renliang.yuan@gmail.com>
2015-04-20 16:45:46 +0800

    i

--- js/javas.js
+++ js/javas.js
@@ -26,15 +26,15 @@ function addJQB(){
 		var tab=document.getElementById("proc"); // 获得表格  
 		var rows=tab.rows; // 表格ROW对象  
 		var row1=tab.insertRow(rows.length); // 插入一行rows是一个数组，代表没一行，索引从0开始  
-		row1.insertCell(0).innerHTML=ItemName[i].getElementsByTagName("序号")[0].childNodes[0].nodeValue; // insertCell插入列，从0开始  
-		row1.insertCell(1).innerHTML=ItemName[i].getElementsByTagName("名称")[0].childNodes[0].nodeValue;  
-		row1.insertCell(2).innerHTML=ItemName[i].getElementsByTagName("素质")[0].childNodes[0].nodeValue;  
-		row1.insertCell(3).innerHTML=ItemName[i].getElementsByTagName("芯片")[0].childNodes[0].nodeValue;
-		row1.insertCell(4).innerHTML=ItemName[i].getElementsByTagName("空间")[0].childNodes[0].nodeValue;
-		row1.insertCell(5).innerHTML=ItemName[i].getElementsByTagName("负重")[0].childNodes[0].nodeValue;
-		row1.insertCell(6).innerHTML=ItemName[i].getElementsByTagName("耐久")[0].childNodes[0].nodeValue;
-		row1.insertCell(7).innerHTML=ItemName[i].getElementsByTagName("耗能")[0].childNodes[0].nodeValue;
-		row1.insertCell(8).innerHTML=ItemName[i].getElementsByTagName("技能")[0].childNodes[0].nodeValue;
-		row1.insertCell(9).innerHTML=ItemName[i].getElementsByTagName("效果")[0].childNodes[0].nodeValue;
+		row1.insertCell(0).innerHTML=Engine[i].getElementsByTagName("序号")[0].childNodes[0].nodeValue; // insertCell插入列，从0开始  
+		row1.insertCell(1).innerHTML=Engine[i].getElementsByTagName("名称")[0].childNodes[0].nodeValue;  
+		row1.insertCell(2).innerHTML=Engine[i].getElementsByTagName("素质")[0].childNodes[0].nodeValue;  
+		row1.insertCell(3).innerHTML=Engine[i].getElementsByTagName("芯片")[0].childNodes[0].nodeValue;
+		row1.insertCell(4).innerHTML=Engine[i].getElementsByTagName("空间")[0].childNodes[0].nodeValue;
+		row1.insertCell(5).innerHTML=Engine[i].getElementsByTagName("负重")[0].childNodes[0].nodeValue;
+		row1.insertCell(6).innerHTML=Engine[i].getElementsByTagName("耐久")[0].childNodes[0].nodeValue;
+		row1.insertCell(7).innerHTML=Engine[i].getElementsByTagName("耗能")[0].childNodes[0].nodeValue;
+		row1.insertCell(8).innerHTML=Engine[i].getElementsByTagName("技能")[0].childNodes[0].nodeValue;
+		row1.insertCell(9).innerHTML=Engine[i].getElementsByTagName("效果")[0].childNodes[0].nodeValue;
 	}
 }
